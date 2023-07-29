edba83e1c1738cdf75f64da4d60d847f06873299
mhmad-alaa <ma2001129@gmail.com>
2023-07-29 21:27:25 +0300

    add starter js

--- script.js
+++ script.js
@@ -1,40 +1,114 @@
-
 // LOGIC
-cases = ["rock", "papper", "scissors"];
+const cases = ['rock', 'paper', 'scissors'];
+const rockDraw = '✊';
+const paperDraw = '✋';
+const scissorsDraw = '✌';
+
+
+let playerScore = 0; 
+let computerScore = 0; 
+
 
 function getComputerChoice() {
     return cases[parseInt(Math.random() * 100) % 3];
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection != computerSelection) {
-
-        if (playerSelection == "rock") {
-            if (computerSelection == "scissors") return 'player';
-            else return "computer"; // papper
-        }
-        else if (playerSelection == "scissors") {
-            if (computerSelection == "papper") return "player";
-            else return "computer"; // rock
-        }
-
-        if (computerSelection == "rock") {
-            if (playerSelection == "scissors") return "computer";
-            else return "player"; // papper
-        }
-        else if (computerSelection == "scissors") {
-            if (playerSelection == "papper") return "computer";
-            else return "player"; // rock
-        }
-    }
-    else return "win-win";
+function isGameOver() {
+    return playerScore == 5 || computerScore == 5; 
 }
 
-const playerSelection = "rock";
-const computerSelection = getComputerChoice();
-console.log(computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+function playRound(playerSelection, computerSelection) {
+    let checkEquality = playerSelection == computerSelection;
+    const prePlayer = playerScore;  
+    const preComputer = computerScore; 
+
+    if (playerSelection == 'rock') {
+        playerSign = rockDraw;
+
+        if (computerSelection == 'scissors') {
+            playerScore++; 
+            computerSign = scissorsDraw;
+        }
+        else {
+            computerScore++; 
+            computerSign = paperDraw; // paper
+        }
+    }
+    else if (playerSelection == 'scissors') {
+        playerSign = scissorsDraw;
+        if (computerSelection == 'paper') {
+            playerScore++; 
+            computerSign = paperDraw;
+        }
+        else {
+            computerScore++; 
+            computerSign = rockDraw; // rock
+        }
+    }
+
+    if (computerSelection == 'rock') {
+        console.log("hello"); 
+        computerSign = rockDraw;
+
+        if (playerSelection == 'scissors') {
+            computerScore++; 
+            playerSign = scissorsDraw;
+        }
+        else {
+            playerScore++; 
+            playerSign = paperDraw; // paper
+        }
+    }
+    else if (computerSelection == 'scissors') {
+        computerSign = scissorsDraw;
+
+        if (playerSelection == 'paper') {
+            computerScore++; 
+            playerSign = paperDraw;
+        }
+        else {
+            playerScore++; 
+            playerSign = rockDraw; // rock
+        }
+    }
+
+    if (checkEquality) {
+        playerScore = prePlayer; 
+        computerScore = preComputer;
+    }
+
+    playerSign.textContent = playerSign; 
+    computerSign.textContent = computerSign;
+}
+
+
 
 
 // UI
 
+const rockBtn = document.getElementById('rockBtn')
+const paperBtn = document.getElementById('paperBtn')
+const scissorsBtn = document.getElementById('scissorsBtn')
+
+// const rock = document.getElementById('rockBtn'); 
+// const paper = document.getElementById('rockBtn'); 
+// const scissors = document.getElementById('scissorsBtn');
+const playerSign = document.getElementById('playerSign');
+const computerSign = document.getElementById('computerSign');
+
+
+rockBtn.addEventListener('click', () => handleClick('rock'));
+paperBtn.addEventListener('click', () => handleClick('paper'));
+scissorsBtn.addEventListener('click', () => handleClick('scissors'));
+
+
+function handleClick(playerSelection) {
+    if (isGameOver()) {
+        return;
+    }
+
+    let computerSelection = getComputerChoice();
+    playRound(playerSelection, computerSelection);
+}
+
+  
