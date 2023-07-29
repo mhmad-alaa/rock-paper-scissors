6c568f47bde28bfb00f6ac3a9a18a2e80383e34b
mhmad-alaa <ma2001129@gmail.com>
2023-07-29 14:50:57 +0300

    feat: add UI

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-cases = ["rock", "papper", "scissors"]; 
+
+// LOGIC
+cases = ["rock", "papper", "scissors"];
 
 function getComputerChoice() {
     return cases[parseInt(Math.random() * 100) % 3];
@@ -8,29 +10,31 @@ function playRound(playerSelection, computerSelection) {
     if (playerSelection != computerSelection) {
 
         if (playerSelection == "rock") {
-            if (computerSelection == "scissors") return 'player'; 
+            if (computerSelection == "scissors") return 'player';
             else return "computer"; // papper
         }
         else if (playerSelection == "scissors") {
-            if (computerSelection == "papper") return "player"; 
+            if (computerSelection == "papper") return "player";
             else return "computer"; // rock
         }
 
         if (computerSelection == "rock") {
-            if (playerSelection == "scissors") return "computer"; 
+            if (playerSelection == "scissors") return "computer";
             else return "player"; // papper
         }
         else if (computerSelection == "scissors") {
-            if (playerSelection == "papper") return "computer"; 
+            if (playerSelection == "papper") return "computer";
             else return "player"; // rock
         }
-    } 
-    else return "win-win"; 
+    }
+    else return "win-win";
 }
-   
+
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-console.log(computerSelection); 
+console.log(computerSelection);
 console.log(playRound(playerSelection, computerSelection));
 
+
+// UI
 
